e2c73ec486cc3ace4b33b5eaba8849f948824661
Laura Dias <lauradiasdesouza04@gmail.com>
2025-04-09 09:25:46 -0300

    correção alunos_rotas.py

--- alunos/alunos_rotas.py
+++ alunos/alunos_rotas.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from .alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, adicionar_aluno, atualizar_aluno, excluir_aluno, alunos
+from .alunos_model import AlunoNaoEncontrado, listar_alunos, aluno_por_id, adicionar_aluno, atualizar_aluno, excluir_aluno
 
 alunos_blueprint = Blueprint('alunos', __name__)
 
@@ -15,38 +15,23 @@ def get_aluno(id_aluno):
     except AlunoNaoEncontrado:
         return jsonify({'message': 'Aluno não encontrado'}), 404
 
-aluno_id_controle = 1  
-
 @alunos_blueprint.route('/alunos', methods=['POST'])
-def adicionar_aluno():
+def post_aluno():
     try:
         data = request.get_json()
         if not data:
-            return jsonify({"message": "Dados inválidos ou faltando no corpo da requisição."}), 400
-        
-        global aluno_id_controle
-        novo_aluno = {
-            "id": aluno_id_controle,  
-            "nome": data["nome"],
-            "idade": data["idade"],
-            "turma_id": data.get("turma_id"),
-            "data_nascimento": data.get("data_nascimento"),
-            "nota_primeiro_semestre": float(data["nota_primeiro_semestre"]),
-            "nota_segundo_semestre": float(data["nota_segundo_semestre"]),
-            "media_final": float(data["media_final"])
-        }
-        alunos["alunos"].append(novo_aluno)  
-        aluno_id_controle += 1
-        return jsonify(novo_aluno), 201 
+            return jsonify({"message": "Dados inválidos ou ausentes"}), 400
+        novo_aluno = adicionar_aluno(data)
+        return jsonify(novo_aluno), 201
     except KeyError as e:
         return jsonify({"message": f"Faltando campo obrigatório: {str(e)}"}), 400
     except ValueError as e:
         return jsonify({"message": f"Erro ao processar os dados: {str(e)}"}), 400
-    
+
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods=['PUT'])
-def update_aluno(id_aluno):
-    data = request.get_json()
+def put_aluno(id_aluno):
     try:
+        data = request.get_json()
         aluno = atualizar_aluno(id_aluno, data)
         return jsonify({"message": "Aluno atualizado com sucesso!", "aluno": aluno})
     except AlunoNaoEncontrado:
